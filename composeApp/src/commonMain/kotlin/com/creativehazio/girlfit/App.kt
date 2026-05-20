a35d6f34868a5402f778560266514e3585ac3521
HazioTheDev <ezekieldavid036@gmail.com>
2026-05-20 08:35:54 +0100

    Home screen impl (#10)
    * Created routes and set up App Nav structure
    
    * Renamed common components to include app's name
    
    * Added core:navigation module to abstract nav logic
    
    * Built and styled bottom nav bar
    
    * Finished setting up auth flow and navigation to main screen
    
    * Completed scaffold of HomeScreen UI
    
    ---------
    
    Co-authored-by: hazio <creativehazio@gmail.com>

--- composeApp/src/commonMain/kotlin/com/creativehazio/girlfit/App.kt
+++ composeApp/src/commonMain/kotlin/com/creativehazio/girlfit/App.kt
@@ -27,7 +27,8 @@ import com.creativehazio.designsystem.components.GirlFitBottomBar
 import com.creativehazio.designsystem.theme.GirlFitTheme
 import com.creativehazio.girlfit.MainAppContainer
 import com.creativehazio.girlfit.navConfig
-import com.creativehazio.home.presentation.HomeScreen
+import com.creativehazio.home.presentation.HomeScreenRoot
+import com.creativehazio.home.presentation.HomeViewModel
 import com.creativehazio.navigation.EmailVerification
 import com.creativehazio.navigation.Home
 import com.creativehazio.navigation.Login
@@ -212,13 +213,18 @@ fun MainAppContainer(
             )
         }
     ) { innerPadding ->
+        //TODO: Add onback to only close app when its homescreen
         NavDisplay(
-            modifier = Modifier.padding(innerPadding),
             backStack = tabBackStack,
             entryProvider = entryProvider {
                 entry<Home> {
-                    HomeScreen(
-                        "Hazio TheDev"
+                    val homeViewModel : HomeViewModel = koinViewModel()
+                    HomeScreenRoot(
+                        contentPaddingValues = innerPadding,
+                        homeViewModel = homeViewModel,
+                        onNavigateToWorkoutDetail = {
+
+                        }
                     )
                 }
 
