93fec9b7cf0b3f30ec4ca693d5b73d295bd93e8f
hazio <creativehazio@gmail.com>
2026-06-05 19:19:14 +0100

    Implemented Meals Filter card

--- feature/meals/src/commonMain/kotlin/com/creativehazio/meals/presentation/meal/MealsScreen.kt
+++ feature/meals/src/commonMain/kotlin/com/creativehazio/meals/presentation/meal/MealsScreen.kt
@@ -1,45 +1,63 @@
 package com.creativehazio.meals.presentation.meal
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.window.PopupProperties
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil3.compose.AsyncImage
 import com.creativehazio.designsystem.components.GirlFitInfoBubble
 import com.creativehazio.designsystem.components.GirlFitSearchBar
 import com.creativehazio.designsystem.theme.Sizing
 import com.creativehazio.designsystem.theme.Spacing
 import girlfit.feature.meals.generated.resources.Res
 import girlfit.feature.meals.generated.resources.camera_track_calories
+import girlfit.feature.meals.generated.resources.down_arrow
 import girlfit.feature.meals.generated.resources.gradient_cam
 import girlfit.feature.meals.generated.resources.meals_search_hint
 import girlfit.feature.meals.generated.resources.meals_suggestions
+import girlfit.feature.meals.generated.resources.pink_check_icon
+import girlfit.feature.meals.generated.resources.premium_icon
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
@@ -54,7 +72,7 @@ fun MealsScreenRoot(
 
     LaunchedEffect(viewModel.effect) {
         viewModel.effect.collect {
-            when(it){
+            when (it) {
                 is MealsEffect.NavigateToMealsDetail -> {
                     onNavigateToMealDetail(it.mealId)
                 }
@@ -75,52 +93,68 @@ internal fun MealsScreen(
     onEvent: (MealsEvent) -> Unit,
     paddingValues: PaddingValues
 ) {
+    var showMealFilterCard by remember { mutableStateOf(false) }
 
-    LazyVerticalGrid(
-        modifier = Modifier.padding(horizontal = Spacing.Medium),
-        columns = GridCells.Fixed(2),
-        verticalArrangement = Arrangement.spacedBy(Spacing.Medium),
-        horizontalArrangement = Arrangement.spacedBy(Spacing.Medium),
-        contentPadding = paddingValues
-    ) {
+    Box(modifier = Modifier.fillMaxSize()) {
 
-        item(
-            span = { GridItemSpan(maxLineSpan) },
+        LazyVerticalGrid(
+            modifier = Modifier.padding(horizontal = Spacing.Medium),
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.spacedBy(Spacing.Medium),
+            horizontalArrangement = Arrangement.spacedBy(Spacing.Medium),
+            contentPadding = paddingValues
         ) {
-            HeaderSection()
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                HeaderSection()
+            }
+
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                Spacer(Modifier.size(54.dp))
+            }
+
+            item(span = { GridItemSpan(maxLineSpan) }) {
+                SuggestionsHeadingAndSearch(
+                    searchQuery = uiState.searchQuery,
+                    showFilterCard = showMealFilterCard,
+                    onSearchQueryChanged = { onEvent(MealsEvent.OnSearchQueryChanged(it)) },
+                    onSearchPressed = { onEvent(MealsEvent.OnSearchPressed) },
+                    mealFilters = uiState.mealFilters,
+                    filterItemIds = uiState.filterItemIds,
+                    onFilterClicked = { showMealFilterCard = !showMealFilterCard },
+                    onFilterItemClicked = {
+                        onEvent(MealsEvent.OnFilterItemClicked(it))
+                    },
+                    onDismissFilter = {
+                        showMealFilterCard = false
+                        onEvent(MealsEvent.ApplyFilters)
+                    }
+                )
+            }
+
+            items(items = uiState.meals, key = { it.id }) {
+                MealCard(
+                    imageUrl = it.imageUrl,
+                    onMealCardClicked = { onEvent(MealsEvent.OnMealCardClicked(it.id)) }
+                )
+            }
         }
 
-        item(
-            span = { GridItemSpan(maxLineSpan) },
-        ) {
-            Spacer(Modifier.size(54.dp))
-        }
-
-        item(
-            span = { GridItemSpan(maxLineSpan) },
-        ) {
-            SuggestionsHeadingAndSearch(
-                searchQuery = uiState.searchQuery,
-                onSearchQueryChanged = {
-
-                },
-                onSearchPressed = {
-
-                }
+        if (showMealFilterCard) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.onSurface.copy(alpha = 0.4f))
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = {
+                            showMealFilterCard = false
+                            onEvent(MealsEvent.ApplyFilters)
+                        }
+                    )
             )
         }
-
-        items(items = uiState.meals, key = { it.id }) {
-            MealCard(
-                imageUrl = it.imageUrl,
-                onMealCardClicked = {
-                    onEvent(MealsEvent.OnMealCardClicked(it.id))
-                }
-            )
-        }
-
     }
-
 }
 
 @Composable
@@ -132,7 +166,11 @@ internal fun HeaderSection() {
             modifier = Modifier.align(Alignment.TopEnd),
             onClick = {}
         ) {
-            Box(modifier = Modifier.size(24.dp).background(Color.Green, CircleShape))
+            Icon(
+                modifier = Modifier.size(Sizing.IconMedium),
+                painter = painterResource(Res.drawable.premium_icon),
+                contentDescription = null
+            )
         }
 
         GirlFitInfoBubble(
@@ -143,7 +181,13 @@ internal fun HeaderSection() {
         Image(
             modifier = Modifier.align(Alignment.TopEnd)
                 .padding(top = 87.dp, end = 180.dp)
-                .size(60.dp),
+                .size(60.dp)
+                .clickable(
+                    indication = null,
+                    interactionSource = remember { MutableInteractionSource() }
+                ) {
+
+                },
             painter = painterResource(Res.drawable.gradient_cam),
             contentDescription = null
         )
@@ -153,32 +197,52 @@ internal fun HeaderSection() {
 @Composable
 internal fun SuggestionsHeadingAndSearch(
     searchQuery: String,
+    showFilterCard: Boolean,
     onSearchQueryChanged: (String) -> Unit,
     onSearchPressed: () -> Unit,
+    mealFilters: List<MealFilter>,
+    filterItemIds: List<String>,
+    onFilterClicked: () -> Unit,
+    onFilterItemClicked: (String) -> Unit,
+    onDismissFilter: () -> Unit
 ) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(Spacing.Medium)
-    ) {
+    Column {
         Text(
             text = stringResource(Res.string.meals_suggestions),
             style = MaterialTheme.typography.headlineSmall,
             color = MaterialTheme.colorScheme.onSurface
         )
 
+        Spacer(Modifier.size(Spacing.Medium))
+
         GirlFitSearchBar(
             query = searchQuery,
-            onQueryChange = {
-                onSearchQueryChanged(it)
-            },
+            onQueryChange = { onSearchQueryChanged(it) },
             placeholderText = stringResource(Res.string.meals_search_hint),
-            onSearchPressed = {
-                onSearchPressed()
-            },
+            onSearchPressed = { onSearchPressed() },
             showFilterIcon = true,
-            onFilterClick = {
-
-            }
+            onFilterClick = onFilterClicked
         )
+
+        if (showFilterCard) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Popup(
+                    alignment = Alignment.TopEnd,
+                    onDismissRequest = { onDismissFilter() },
+                    properties = PopupProperties(focusable = true)
+                ) {
+                    MealsFilterCard(
+                        mealFilters = mealFilters,
+                        filterItemIds = filterItemIds,
+                        onFilterItemClicked = {
+                            onFilterItemClicked(it)
+                        }
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -195,16 +259,9 @@ internal fun MealCard(
                 onMealCardClicked()
             }
     ) {
-//        AsyncImage(
-//            modifier = Modifier.fillMaxSize(),
-//            model = imageUrl,
-//            contentDescription = null,
-//            contentScale = ContentScale.Crop,
-//        )
-
-        Image(
+        AsyncImage(
             modifier = Modifier.fillMaxSize(),
-            painter = painterResource(Res.drawable.gradient_cam),
+            model = imageUrl,
             contentDescription = null,
             contentScale = ContentScale.Crop,
         )
@@ -212,12 +269,103 @@ internal fun MealCard(
 
 }
 
+@Composable
+internal fun MealsFilterCard(
+    mealFilters: List<MealFilter>,
+    filterItemIds: List<String>,
+    onFilterItemClicked: (String) -> Unit,
+) {
+
+    var expandedFilterIndex by remember { mutableIntStateOf(0) }
+
+    Card(
+        shape = MaterialTheme.shapes.large,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background
+        )
+    ) {
+
+        Column(
+            modifier = Modifier.padding(Spacing.Large),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ) {
+            mealFilters.forEachIndexed { index, mealFilter ->
+                val isExpanded = expandedFilterIndex == index
+
+                val arrowRotation by animateFloatAsState(
+                    targetValue = if (isExpanded) 180f else 0f,
+                    label = "arrow_rotation"
+                )
+
+                Row(
+                    modifier = Modifier.clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() }
+                    ) {
+                        expandedFilterIndex = if (isExpanded) expandedFilterIndex else index
+                    },
+                    horizontalArrangement = Arrangement.spacedBy(Spacing.Small),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = mealFilter.name,
+                        color = MaterialTheme.colorScheme.onSurface,
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                    Icon(
+                        modifier = Modifier.graphicsLayer {
+                            rotationZ = arrowRotation
+                        },
+                        painter = painterResource(Res.drawable.down_arrow),
+                        contentDescription = null
+                    )
+                }
+
+                AnimatedVisibility(visible = isExpanded) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(Spacing.Large)
+                    ) {
+                        mealFilter.items.forEach { filterItem ->
+                            Row(
+                                modifier = Modifier.width(200.dp)
+                                    .clickable(
+                                        interactionSource = remember { MutableInteractionSource() },
+                                        indication = null,
+                                    ) {
+                                        onFilterItemClicked(filterItem.id)
+                                    },
+                                horizontalArrangement = Arrangement.spacedBy(Spacing.ExtraSmall),
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Text(
+                                    text = filterItem.name,
+                                    style = MaterialTheme.typography.labelLarge
+                                )
+                                if (filterItemIds.contains(filterItem.id)) {
+                                    Icon(
+                                        painter = painterResource(Res.drawable.pink_check_icon),
+                                        tint = Color.Unspecified,
+                                        contentDescription = null
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+    }
+
+}
+
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 internal fun MealPreview() {
-    MealsScreen(
-        uiState = MealsState(),
-        onEvent = {},
-        paddingValues = PaddingValues.Zero
+    MealsFilterCard(
+        mealFilters = getDummyFilters(),
+        onFilterItemClicked = {},
+        filterItemIds = listOf(),
     )
 }
--- feature/meals/src/commonMain/kotlin/com/creativehazio/meals/presentation/meal/MealsViewModel.kt
+++ feature/meals/src/commonMain/kotlin/com/creativehazio/meals/presentation/meal/MealsViewModel.kt
@@ -1,10 +1,12 @@
 package com.creativehazio.meals.presentation.meal
 
+import androidx.lifecycle.viewModelScope
 import com.creativehazio.common.BaseViewModel
 import com.creativehazio.common.Effect
 import com.creativehazio.common.Event
 import com.creativehazio.common.State
 import com.creativehazio.data.meal.domain.Meal
+import kotlinx.coroutines.launch
 
 data class MealsState(
     val isLoading: Boolean = false,
@@ -42,11 +44,17 @@ data class MealsState(
             id = "8",
             imageUrl = "",
         ),
-    )
+    ),
+    val mealFilters: List<MealFilter> = getDummyFilters(),
+    val filterItemIds: List<String> = emptyList()
 ) : State
 
 sealed interface MealsEvent : Event {
     data class OnMealCardClicked(val mealId: String) : MealsEvent
+    data class OnSearchQueryChanged(val searchQuery: String) : MealsEvent
+    data object OnSearchPressed : MealsEvent
+    data class OnFilterItemClicked(val filterItemId: String) : MealsEvent
+    data object ApplyFilters : MealsEvent
 }
 
 sealed interface MealsEffect : Effect {
@@ -58,6 +66,78 @@ class MealsViewModel(
     MealsState()
 ) {
     override fun onEvent(event: MealsEvent) {
-
+        when(event) {
+            is MealsEvent.OnMealCardClicked -> {}
+            MealsEvent.OnSearchPressed -> {}
+            is MealsEvent.OnSearchQueryChanged -> {
+                updateState { copy(searchQuery = event.searchQuery) }
+            }
+            is MealsEvent.OnFilterItemClicked -> addFilterItem(event.filterItemId)
+            MealsEvent.ApplyFilters -> applyFilters()
+        }
     }
+
+    private fun applyFilters() {
+        viewModelScope.launch {
+
+        }
+    }
+
+    private fun addFilterItem(filterItemId: String) {
+        val filterItemIds = uiState.value.filterItemIds
+        if (filterItemIds.contains(filterItemId)) {
+            updateState { copy(filterItemIds = filterItemIds - filterItemId) }
+        } else {
+            updateState { copy(filterItemIds = filterItemIds + filterItemId) }
+        }
+    }
+}
+
+data class MealFilter(
+    val id: String,
+    val name: String,
+    val items: List<FilterItem>
+)
+
+data class FilterItem(
+    val id: String,
+    val name: String,
+)
+
+fun getDummyFilters() : List<MealFilter> {
+    return listOf(
+        MealFilter(
+            id = "1",
+            name = "Goal",
+            items = listOf(
+                FilterItem(id = "goal_1", name = "Fat loss"),
+                FilterItem(id = "goal_2", name = "Muscle tone & strength"),
+                FilterItem(id = "goal_3", name = "Energy & performance"),
+                FilterItem(id = "goal_4", name = "Hormone & cycle balance"),
+                FilterItem(id = "goal_5", name = "Gut health & digestion")
+            )
+        ),
+        MealFilter(
+            id = "2",
+            name = "Meal time",
+            items = listOf(
+                FilterItem(id = "meal_time_1", name = "Breakfast"),
+                FilterItem(id = "meal_time_2", name = "Lunch"),
+                FilterItem(id = "meal_time_3", name = "Dinner"),
+                FilterItem(id = "meal_time_4", name = "Snacks"),
+                FilterItem(id = "meal_time_5", name = "Smoothies & Drinks")
+            )
+        ),
+        MealFilter(
+            id = "3",
+            name = "Time needed",
+            items = listOf(
+                FilterItem(id = "time_needed_1", name = "Under 15 mins"),
+                FilterItem(id = "time_needed_2", name = "15 - 30 mins"),
+                FilterItem(id = "time_needed_3", name = "30 - 45 mins"),
+                FilterItem(id = "time_needed_4", name = "45 - 60 mins"),
+                FilterItem(id = "time_needed_5", name = "Over 60 mins")
+            )
+        )
+    )
 }
